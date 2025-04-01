ddaf7d266e6e5de98119aa14b8d3c85164a8b315
abledbody <abledbody.gamedev@gmail.com>
2025-03-31 21:12:42 -0400

    Initial commit

---
+++ animation_editor.p64/main.lua
@@ -0,0 +1,8 @@
+-- Development
+cd("/projects/animation_editor")
+DATP = "animation_editor.p64/"
+-- Distribution - Copy `src` folder into `animation_editor.p64`
+--DATP = ""
+
+-- ... --
+include"src/editor.lua"
