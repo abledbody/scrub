b0cd3974d50bdc9379e9b768eee977211a8ca88d
Drew "abledbody" Mudge <abledbody.gamedev@gmail.com>
2025-11-02 20:59:16 -0500

    New development directory

--- scrub.p64/main.lua
+++ scrub.p64/main.lua
@@ -1,7 +1,7 @@
 --[[pod_format="raw",created="2025-04-04 00:01:14",modified="2025-04-04 00:03:56",revision=6]]
 DATP = ""
 if not fetch"src/main.lua" then
-	cd("/projects/scrub")
+	cd("/projects/apps/scrub")
 	DATP = "scrub.p64/"
 end
 include"src/main.lua"
