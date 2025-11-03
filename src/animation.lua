7ae7f3f63409a87f068a231891d09b8d5b2f7722
Drew "abledbody" Mudge <abledbody.gamedev@gmail.com>
2025-11-02 22:55:42 -0500

    Formatting animation.lua

--- src/animation.lua
+++ src/animation.lua
@@ -10,13 +10,14 @@
 --- Advances the time of the animator by dt.
 --- @param self Animator The animator.
 --- @param dt number The time to advance by.
-local function advance(self,dt)
+local function advance(self, dt)
 	self.frame_advances = 0
 	self.ended = false
-
+	
 	local anim = self.anim
 	if not self.anim then return end
-
+	---@cast anim Animation
+	
 	local durations = anim.duration
 	if self.frame_i > #durations then
 		self.frame_i = 1
@@ -24,26 +25,26 @@ local function advance(self,dt)
 	end
 	
 	local events = {}
-
+	
 	self.frame_t += dt
 	local duration = durations[self.frame_i] or durations[1]
-
+	
 	while self.frame_t > duration do
 		self.frame_t -= duration
-
+		
 		self.frame_i += 1
 		self.frame_advances += 1
 		if self.frame_i > #durations then
 			self.frame_i = 1
 			self.ended = true
 		end
-
+		
 		local frame_events = anim.events and anim.events[self.frame_i]
-		if frame_events then add(events,frame_events) end
-
+		if frame_events then add(events, frame_events) end
+		
 		duration = durations[self.frame_i]
 	end
-
+	
 	self.events = events
 end
 
@@ -52,20 +53,20 @@ end
 --- @param self Animator The animator.
 --- @param key any The key to index the animation by.
 --- @return any value The value fetched by the key.
-local function fetch(self,key)
+local function fetch(self, key)
 	local anim = self.anim
 	if not anim then return nil end
 	
 	local arr = anim[key]
 	if not arr then return nil end
-
+	
 	return arr[self.frame_i] or arr[-1]
 end
 
 --- Resets the animator to a specific frame.
 --- @param self Animator The animator.
 --- @param frame number? The frame to reset to. Defaults to 1.
-local function reset(self,frame)
+local function reset(self, frame)
 	self.frame_i = frame or 1
 	self.frame_t = 0
 	self.frame_advances = 0
@@ -74,8 +75,8 @@ local function reset(self,frame)
 end
 
 local c_animator = {
-	__index = function(self,key)
-		return key ~= "anim" and fetch(self,key)
+	__index = function(self, key)
+		return key ~= "anim" and fetch(self, key)
 	end
 }
 
@@ -98,26 +99,26 @@ local function new_animator(anim)
 		frame_advances = 0,
 		ended = false,
 		events = {},
-
+		
 		advance = advance,
 		reset = reset,
 	}
-	return setmetatable(animator,c_animator)
+	return setmetatable(animator, c_animator)
 end
 
 --- Generates an animation by a rule function.
 --- @param length integer The number of frames to add to the animation.
 --- @param rule fun(frame_i:integer):{duration:number} A function which takes in a frame index and returns a table with all the keys and values for that frame. The `duration` key is always required.
 --- @return Animation animation An Animation generated via the provided rule.
-local function animation_by_rule(length,rule)
+local function animation_by_rule(length, rule)
 	local animation = {}
 	for frame_i = 1, length do
-		for k,v in pairs(rule(frame_i)) do
+		for k, v in pairs(rule(frame_i)) do
 			animation[k] = animation[k] or {}
 			animation[k][frame_i] = v
 		end
 	end
-
+	
 	return animation
 end
 
